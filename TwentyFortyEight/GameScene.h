79796c41b1ac3652bbd8c18c6244420ab5d55ea2
Josh Caswell <josh@kraken>
2016-02-28 14:03:32 -0800

    Initial Commit

---
+++ TwentyFortyEight/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  TwentyFortyEight
+//
+
+//  Copyright (c) 2016 Josh Caswell. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ TwentyFortyEight/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TwentyFortyEight
+//
+//  Created by Joshua Caswell on 2/28/16.
+//  Copyright © 2016 Josh Caswell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
